18c42be36d1f2d49534a4602e3f0addb5c070adb
Tim Sherratt <tim@discontents.com.au>
2018-01-30 11:39:31 +1100

    Add data files

--- changes.py
+++ changes.py
@@ -12,6 +12,33 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 
 
+def find_date_changes(old=MONGOLAB_URL_2018, new=MONGOLAB_URL_2017):
+    '''
+    Files that were closed in both harvests but had different decision dates.
+    Probably becuase they were WPA in old harvest.
+    '''
+    old_client = MongoClient(old)
+    old_db = old_client.get_default_database()
+    item_client = RSItemClient()
+    new_client = MongoClient(new)
+    new_db = new_client.get_default_database()
+    date_changed = 0
+    changed = 0
+    for item_old in old_db.items.find().batch_size(50):
+        barcode = item_old['identifier']
+        item_new = new_db.items.find_one({'_id': barcode})
+        if item_new:
+            if item_old['access_decision']['start_date']['date'] != item_new['access_decision']['start_date']['date']:
+                print barcode
+                date_changed += 1
+        else:
+            if item_old['access_decision']['start_date']['date'] >= datetime(2017, 1, 1, 0, 0, 0):
+                changed += 1
+                print '{}: {}, {}'.format(item_old['access_decision']['start_date']['date'], item_old['series'], item_old['_id'])
+    print date_changed
+    print changed
+
+
 def find_changes(old=MONGOLAB_URL_2017, new=MONGOLAB_URL_2018):
     # dbclient2016 = MongoClient(MONGOLAB_URL_2016)
     # db2016 = dbclient2016.get_default_database()
@@ -98,38 +125,44 @@ def save_changed_data(dbase=MONGOLAB_URL_2018, access_status=None, reason=None, 
                 years, days = divmod(period.days, 365.25)
                 period_days = period.days
             except (TypeError, KeyError):
-                # Some broken record...
-                pass
-            else:
-                if record['access_decision']['start_date']['date'].year != 1900 and record['current']['access_decision']['start_date']['date'].year != 1900:
-                    total_days += period.days
-                    count += 1
-                titles.writerow([
-                    record['current']['identifier'],
-                    record['current']['series'],
-                    record['current']['control_symbol'],
-                    record['current']['title'],
-                    record['current']['contents_dates']['date_str'].encode('utf-8'),
-                    record['current']['contents_dates']['start_date']['date'].year,
-                    record['current']['contents_dates']['end_date']['date'].year,
-                    record['access_status'].encode('utf-8'),
-                    ' | '.join(record['reasons']),
-                    record['access_decision']['start_date']['date'],
-                    record['current']['access_status'].encode('utf-8'),
-                    ' | '.join(record['current']['reasons']),
-                    record['current']['access_decision']['start_date']['date'],
-                    period_days,
-                    '{} years {} days'.format(int(years), int(round(days))),
-                    'http://www.naa.gov.au/cgi-bin/Search?O=I&Number={}'.format(record['current']['identifier'])
-                    ])
+                # Missing records!
+                period_days = 0
+                days = 0
+            if record['access_decision']['start_date']['date'].year != 1900 and record['current']['access_decision']['start_date']['date'].year != 1900:
+                total_days += period.days
+                count += 1
+            titles.writerow([
+                record['current']['identifier'],
+                record['current']['series'],
+                record['current']['control_symbol'],
+                record['current']['title'].encode('utf-8'),
+                record['current']['contents_dates']['date_str'].encode('utf-8'),
+                record['current']['contents_dates']['start_date']['date'].year,
+                record['current']['contents_dates']['end_date']['date'].year,
+                record['access_status'].encode('utf-8'),
+                ' | '.join(record['reasons']),
+                record['access_decision']['start_date']['date'],
+                record['current']['access_status'].encode('utf-8'),
+                ' | '.join(record['current']['reasons']),
+                record['current']['access_decision']['start_date']['date'],
+                period_days,
+                '{} years {} days'.format(int(years), int(round(days))),
+                'http://www.naa.gov.au/cgi-bin/Search?O=I&Number={}'.format(record['current']['identifier'])
+                ])
     print total_days
     average_days = total_days / count
     years, days = divmod(average_days, 365.25)
     print 'Average period closed: {} days ({} years {} days)'.format(average_days, int(years), int(round(days)))
 
 
-def get_reason_changes(db):
+def get_reason_changes(db, exclude_missing=True, exclude_wpa=False):
+    query = {}
+    if exclude_missing:
+        query["update.access_status"] = {"$ne": "Missing"}
+    if exclude_wpa:
+        query["reasons"] = {"$nin": ["Withheld pending adv"]}
     pipeline = [
+            {"$match": query},
             {"$unwind": "$reasons"},
             {"$group": {"_id": "$reasons", "total": {"$sum": 1}}},
             {"$project": {"_id": 0, "reason": "$_id", "total": "$total"}},
@@ -149,7 +182,7 @@ def count_changes(db=MONGOLAB_URL_2018, reason=None, series=None, decision_year=
     if decision_year:
         decision_date_start = datetime(decision_year, 1, 1, 0, 0, 0)
         decision_date_end = datetime(decision_year, 12, 31, 0, 0, 0)
-        query['access_decision.start_date.date'] = {'$gte': decision_date_start, '$lte': decision_date_end}
+        query['current.access_decision.start_date.date'] = {'$gte': decision_date_start, '$lte': decision_date_end}
     print query
     pipeline = [
         {'$match': query},
@@ -176,11 +209,13 @@ def get_series(db, reason=None, decision_year=None):
     return list(db.items.aggregate(pipeline))[:20]
 
 
-def get_reasons(db, decision_year=None):
+def get_reasons(db, decision_year=None, exclude_wpa=False):
     '''
     Get the normalised reasons and count.
     '''
     query = {}
+    if exclude_wpa:
+        query["reasons"] = {"$nin": ["Withheld pending adv"]}
     if decision_year:
         decision_date_start = datetime(decision_year, 1, 1, 0, 0, 0)
         decision_date_end = datetime(decision_year, 12, 31, 0, 0, 0)
@@ -196,11 +231,19 @@ def get_reasons(db, decision_year=None):
     return list(items.aggregate(pipeline))
 
 
-def plot_reasons_changes(dbase={'year': '2017', 'db': MONGOLAB_URL_2018}):
+def plot_reasons_changes(dbase={'year': '2017', 'db': MONGOLAB_URL_2018}, exclude_wpa=False):
     client = MongoClient(dbase['db'])
     db = client.get_default_database()
-    reasons = get_reasons(db, 2017)
-    changes = get_reason_changes(db)
+    if exclude_wpa:
+        reasons = get_reasons(db, 2017, exclude_wpa=True)
+        changes = get_reason_changes(db, exclude_wpa=True)
+        filename = 'totals-by-reason-changes-excluding-wpa{}'.format(dbase['year'])
+        title = '<b>Closed files</b><br>Changes in {} (excluding \'Withheld pending adv\')'.format(dbase['year'])
+    else:
+        reasons = get_reasons(db, 2017)
+        changes = get_reason_changes(db)
+        filename = 'totals-by-reason-changes-{}'.format(dbase['year'])
+        title = '<b>Closed files</b><br>Changes in {}'.format(dbase['year'])
     added = []
     changed = []
     x = sorted(list(set([reason['reason'] for reason in reasons] + [reason['reason'] for reason in changes])))
@@ -211,16 +254,16 @@ def plot_reasons_changes(dbase={'year': '2017', 'db': MONGOLAB_URL_2018}):
         go.Bar(
             x=x,
             y=added,
-            name='Added'
+            name='Closed'
         ),
         go.Bar(
             x=x,
             y=changed,
-            name='Removed'
+            name='Un-closed'
         )
     ]
     layout = go.Layout(
-        title='<b>Closed files</b><br>Changes in {}'.format(dbase['year']),
+        title=title,
         barmode='relative',
         xaxis=go.XAxis(
             title='Reason the file is closed'
@@ -236,7 +279,50 @@ def plot_reasons_changes(dbase={'year': '2017', 'db': MONGOLAB_URL_2018}):
         ),
     )
     fig = go.Figure(data=data, layout=layout)
-    plot_url = py.plot(fig, filename='totals-by-reason-changes-{}'.format(dbase['year']))
+    plot_url = py.plot(fig, filename=filename)
+
+
+def plot_reasons_changes_nowpa(dbase={'year': '2017', 'db': MONGOLAB_URL_2018}):
+    client = MongoClient(dbase['db'])
+    db = client.get_default_database()
+    reasons = get_reasons(db, 2017, exclude_wpa=True)
+    changes = get_reason_changes(db, exclude_wpa=True)
+    added = []
+    changed = []
+    x = sorted(list(set([reason['reason'] for reason in reasons] + [reason['reason'] for reason in changes])))
+    for key in x:
+        added.append(get_total(reasons, key))
+        changed.append(0 - get_total(changes, key))
+    data = [
+        go.Bar(
+            x=x,
+            y=added,
+            name='Closed'
+        ),
+        go.Bar(
+            x=x,
+            y=changed,
+            name='Un-closed'
+        )
+    ]
+    layout = go.Layout(
+        title='<b>Closed files</b><br>Changes in {} (excluding \'Withheld pending adv\')'.format(dbase['year']),
+        barmode='relative',
+        xaxis=go.XAxis(
+            title='Reason the file is closed'
+        ),
+        yaxis=go.YAxis(
+            title='Number of files'
+        ),
+        margin=dict(
+            l=100,
+            r=100,
+            t=100,
+            b=180
+        ),
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plot_url = py.plot(fig, filename='totals-by-reason-changes-excluding-wpa{}'.format(dbase['year']))
 
 
 def get_total(results, key):
